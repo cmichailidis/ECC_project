2db05a3d838e1ed94c85f4d63d6a2a3fc5cf57c0
Christodoulos Michaelides <cmichailidis98@gmail.com>
2023-06-18 23:47:11 +0300

    Delete bsc.m

--- bsc.m
+++
@@ -1,8 +0,0 @@
-function y = bsc (x, epsilon)
-  noise_mask = rand(size(x));
-  noise_mask = noise_mask <= epsilon;
-
-  y = zeros(size(x));
-  y(noise_mask == 1) = ~x(noise_mask == 1);
-  y(noise_mask == 0) =  x(noise_mask == 0);
-end
